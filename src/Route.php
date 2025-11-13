d3e96b2c85a0ac39b43ad6233385ae7a7031c1ee
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2025-11-13 12:26:12 +0300

    [fix]: Added method constants

--- src/Route.php
+++ src/Route.php
@@ -8,6 +8,27 @@ use RuntimeException;
 
 final class Route
 {
+    const METHOD_GET = 'GET';
+    const METHOD_HEAD = 'HEAD';
+    const METHOD_POST = 'POST';
+    const METHOD_PATCH = 'PATCH';
+    const METHOD_PUT = 'PUT';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_OPTIONS = 'OPTIONS';
+
+    /**
+     * @var string[]
+     */
+    private static array $supportedMethod = [
+        self::METHOD_GET,
+        self::METHOD_HEAD,
+        self::METHOD_POST,
+        self::METHOD_PATCH,
+        self::METHOD_PUT,
+        self::METHOD_DELETE,
+        self::METHOD_OPTIONS,
+    ];
+
     /**
      * @var array
      */
@@ -23,7 +44,7 @@ final class Route
         public string $pattern,
         public string $handler,
     ) {
-        if (!in_array($method, ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'])) {
+        if (!in_array($method, self::$supportedMethod)) {
             throw new RuntimeException(sprintf("Method '%s' not supported!", $method), 500);
         }
 
